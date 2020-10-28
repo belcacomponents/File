57d4e98a730aebf739d27b1e8bfec8021f32068e
Oleg Dmitrochenko <dmitrochenkooleg@gmail.com>
2020-10-28 21:23:16 +0300

    add publication

--- src/FileServiceProvider.php
+++ src/FileServiceProvider.php
@@ -14,6 +14,10 @@ class FileServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        $this->publishes([
+            __DIR__.'/../database/migrations' => database_path('migrations')
+        ], 'file-migration');
     }
 
     /**
